56c861e49ec0a832f54008fcc10915a7ddcf9896
Indro Lie <Indrolie@gmail.com>
2018-08-27 21:33:28 +0700

    Add carousel for homepage "review" section

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,60 +1,79 @@
 import React, { Component } from 'react'
 import { Button, Header, Menu, Segment, Sidebar } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 import MyNavigation from '../../Navigationbar'
 import Landing from '../../Landing'
 import About from '../../About'
+import Review from '../../Review'
 
 // import './index.css'
 
 export default class Home extends Component {
-    state = { visible: false }
+  state = { visible: false }
 
-    handleButtonClick = () => this.setState({ visible: !this.state.visible })
+  handleButtonClick = () => this.setState({ visible: !this.state.visible })
 
-    handleSidebarHide = () => this.setState({ visible: false })
+  handleSidebarHide = () => this.setState({ visible: false })
 
-    render() {
-      const { visible } = this.state
+  render() {
+    const { visible } = this.state
 
-      return (
-        <div>
-          <MyNavigation>
-            <Button icon='bars' onClick={this.handleButtonClick} className='button-menu' />
-          </MyNavigation>
+    return (
+      <div>
+        <MyNavigation>
+          <Button icon='bars' onClick={this.handleButtonClick} className='button-menu' />
+        </MyNavigation>
 
-          <Sidebar.Pushable as={Segment}>
-            <Sidebar
-              as={Menu}
-              animation='overlay'
-              icon='labeled'
-              inverted
-              onHide={this.handleSidebarHide}
-              vertical
-              visible={visible}
-              width='thin'
-            >
-              <Menu.Item as='a'>
-                            Home
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar
+            as={Menu}
+            animation='overlay'
+            icon='labeled'
+            inverted
+            onHide={this.handleSidebarHide}
+            vertical
+            visible={visible}
+            width='thin'
+          >
+            <Link to='/'>
+              <Menu.Item>
+                Home
               </Menu.Item>
-              <Menu.Item as='a'>
-                            About us
+            </Link>
+            <Link to='/about'>
+              <Menu.Item>
+                About us
               </Menu.Item>
-              <Menu.Item as='a'>
-                            Contact us
+            </Link>
+            <Link to='/contact'>
+              <Menu.Item>
+                Contact us
               </Menu.Item>
-            </Sidebar>
+            </Link>
+            <Link to='/merchants/register'>
+              <Menu.Item>
+                Register merchant
+              </Menu.Item>
+            </Link>
+            <Link to='users/register'>
+              <Menu.Item>
+                Register user
+              </Menu.Item>
+            </Link>
+          </Sidebar>
 
-            <Sidebar.Pusher>
-              <Segment basic>
-                <Header as='h3'>
-                  <Landing />
-                  <About />
-                </Header>
-              </Segment>
-            </Sidebar.Pusher>
-          </Sidebar.Pushable>
-        </div>
-      )
-    }
+          <Sidebar.Pusher>
+            <Segment basic>
+              <Header as='h3'>
+                <Landing />
+                <Review />
+                <About />
+              </Header>
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </div >
+    )
+  }
 }
---
+++ src/Review/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Carousel from 'nuka-carousel'
+
+export default class Review extends React.Component {
+  render() {
+    return (
+      <Carousel>
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1" />
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2" />
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3" />
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4" />
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5" />
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6" />
+      </Carousel>
+    )
+  }
+}
