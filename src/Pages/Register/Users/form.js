b155cb835ce37e598d0d022f8ca942b34c1e726c
Indro Lie <Indrolie@gmail.com>
2018-08-28 15:16:26 +0700

    Integrate registration page with backend

--- src/Pages/Register/Users/form.js
+++ src/Pages/Register/Users/form.js
@@ -1,41 +1,79 @@
 import React from 'react'
 import { Container, Button, Form, Checkbox } from 'semantic-ui-react'
 
+import Register from '../../../function/register'
+
 import './index.css'
 
-const RegisterUsers = () => (
-  <div className=''>
-    <label className='daftar-users'>Registrasi Pembeli</label>
-    <Container>
-      <Form className='form-register-users'>
-        <Form.Field>
-          <label>Username</label>
-          <Form.Input placeholder='Username' />
-        </Form.Field>
-        <Form.Field>
-          <label>Email</label>
-          <Form.Input placeholder='Email' />
-        </Form.Field>
-        <Form.Field>
-          <label>Kata Sandi</label>
-          <Form.Input type='password' placeholder='Kata Sandi' />
-        </Form.Field>
-        <Form.Field>
-          <label>Nama Lengkap</label>
-          <Form.Input type='password' placeholder='Kata Sandi' />
-        </Form.Field>
-        <Form.Field>
-          <label>Nomor telepon</label>
-          <Form.Input type='password' placeholder='Kata Sandi' />
-        </Form.Field>
-        <Form.TextArea label='Alamat' placeholder='Alamat' />
-        <Form.Field>
-          <Checkbox label='Saya setuju dengan persyaratan dan ketentuan galonku.com' />
-        </Form.Field>
-        <Button type='submit'>Registrasi</Button>
-      </Form>
-    </Container>
-  </div>
-)
+class RegisterUsers extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      fullname: '',
+      phone_number: '',
+      address: ''
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  handleSubmit = (event) => {
+
+    const URL = 'users/register'
+    const data = {
+      username: this.state.username,
+      email: this.state.email,
+      password: this.state.password,
+      fullname: this.state.fullname,
+      phone_number: this.state.phone_number,
+      address: this.state.address
+    }
+
+    event.preventDefault()
+    Register(URL, data)
+  }
+
+  render() {
+    return (
+      <div className=''>
+        <label className='daftar-users'>Registrasi Pembeli</label>
+        <Container>
+          <Form className='form-register-users' onSubmit={this.handleSubmit}>
+            <Form.Field>
+              <label>Username</label>
+              <Form.Input name='username' type='text' placeholder='Username' onChange={this.handleChange} />
+            </Form.Field>
+            <Form.Field>
+              <label>Email</label>
+              <Form.Input name='email' type='email' placeholder='Email' onChange={this.handleChange} />
+            </Form.Field>
+            <Form.Field>
+              <label>Kata Sandi</label>
+              <Form.Input name='password' type='password' placeholder='Kata Sandi' onChange={this.handleChange} />
+            </Form.Field>
+            <Form.Field>
+              <label>Nama Lengkap</label>
+              <Form.Input name='fullname' type='text' placeholder='Nama Lengkap' onChange={this.handleChange} />
+            </Form.Field>
+            <Form.Field>
+              <label>Nomor telepon</label>
+              <Form.Input name='phone_number' type='text' placeholder='Nomor telepon' onChange={this.handleChange} />
+            </Form.Field>
+            <Form.TextArea label='Alamat' name='address' type='text' placeholder='Alamat' onChange={this.handleChange} />
+            <Form.Field>
+              <Checkbox label='Saya setuju dengan persyaratan dan ketentuan galonku.com' />
+            </Form.Field>
+            <Button type='submit' onClick={this.handleClick}>Registrasi</Button>
+          </Form>
+        </Container>
+      </div>
+    )
+  }
+}
 
 export default RegisterUsers
