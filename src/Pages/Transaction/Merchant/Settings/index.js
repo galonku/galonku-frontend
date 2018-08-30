9f6705129cab02302a8326866b8d837b9e0d068e
unknown <email@abbayosua.com>
2018-08-31 03:13:29 +0700

    Added Footer on Merchant Settings Panel

--- src/Pages/Transaction/Merchant/Settings/index.js
+++ src/Pages/Transaction/Merchant/Settings/index.js
@@ -3,6 +3,7 @@ import { Button, Header, Divider, Icon, Form } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 import MenuLogin from '../../../../MenuLogin'
+import Footer from '../../../../Footer'
 import { getLocalstorage } from '../../../../function/Localstorage'
 import updateMerchants from '../../../../function/UpdateMerchants'
 
@@ -50,52 +51,54 @@ export default class MerchantOpen extends Component {
   render() {
     return (
       <MenuLogin>
-        <div className='button-settings'>
-          <Button
-            onClick={this.props.history.goBack}
-            basic
-            color='grey'
-            content='Kembali Ke awal'
-            icon='backward'
-          />
+        <div className='content-container'>
+          <div className='button-settings'>
+            <Button
+              onClick={this.props.history.goBack}
+              basic
+              color='grey'
+              content='Kembali Ke awal'
+              icon='backward'
+            />
+          </div>
+          <Header as='h3' icon textAlign='center'>
+            <Icon name='users' circular />
+            <Header.Content>Nama Penjual</Header.Content>
+          </Header>
+          <Divider />
+          <Header as='h2' className='order-status'>Pengaturan</Header>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Field>
+              <b>Harga per Galon (Rp)</b>
+              <Form.Input name='price' value={this.state.price} onChange={this.handleChange} />
+            </Form.Field>
+            <Form.Field>
+              <b>Ganti Nama Toko</b>
+              <Form.Input name='store_name' type='text' value={this.state.store_name} onChange={this.handleChange} />
+            </Form.Field>
+            <Form.Field>
+              <b>Ganti Alamat Toko</b>
+              <Form.Input name='address' type='text' value={this.state.address} onChange={this.handleChange} />
+            </Form.Field>
+            <Form.Field>
+              <b>Ganti Email</b>
+              <Form.Input name='email' value={this.state.email} onChange={this.handleChange} />
+            </Form.Field>
+            <Form.Field>
+              <b>Ganti Nomor telepon</b>
+              <Form.Input name='phone_number' value={this.state.phone_number} onChange={this.handleChange} />
+            </Form.Field>
+            <Form.Field>
+              <b>Ganti Kata Sandi</b>
+              <Form.Input name='password' type='password' value={this.state.password} onChange={this.handleChange} />
+            </Form.Field>
+            <Button
+              basic
+              content='Perbaharui'
+            />
+          </Form>
         </div>
-        <Header as='h3' icon textAlign='center'>
-          <Icon name='users' circular />
-          <Header.Content>Nama Penjual</Header.Content>
-        </Header>
-        <Divider />
-        <Header as='h2' className='order-status'>Pengaturan</Header>
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Field>
-            <b>Harga per Galon (Rp)</b>
-            <Form.Input name='price' value={this.state.price} onChange={this.handleChange} />
-          </Form.Field>
-          <Form.Field>
-            <b>Ganti Nama Toko</b>
-            <Form.Input name='store_name' type='text' value={this.state.store_name} onChange={this.handleChange} />
-          </Form.Field>
-          <Form.Field>
-            <b>Ganti Alamat Toko</b>
-            <Form.Input name='address' type='text' value={this.state.address} onChange={this.handleChange} />
-          </Form.Field>
-          <Form.Field>
-            <b>Ganti Email</b>
-            <Form.Input name='email' value={this.state.email} onChange={this.handleChange} />
-          </Form.Field>
-          <Form.Field>
-            <b>Ganti Nomor telepon</b>
-            <Form.Input name='phone_number' value={this.state.phone_number} onChange={this.handleChange} />
-          </Form.Field>
-          <Form.Field>
-            <b>Ganti Kata Sandi</b>
-            <Form.Input name='password' type='password' value={this.state.password} onChange={this.handleChange} />
-          </Form.Field>
-          <Button
-            basic
-            content='Perbaharui'
-          />
-        </Form>
-
+        <Footer />
       </MenuLogin>
     )
   }
