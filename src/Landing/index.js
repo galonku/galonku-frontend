ba79c15ce2e91349d531b2f3164a67c4bd34a30c
unknown <email@abbayosua.com>
2018-08-30 15:52:52 +0700

    Change the fonts style on landing page

--- src/Landing/index.js
+++ src/Landing/index.js
@@ -6,14 +6,17 @@ import './index.css'
 
 const Landing = () => (
   <div className='landing'>
-    <div className='tagline'>Pesan dan Jual Galon Lebih Mudah</div>
-    <div className='button-landing'>
-      <Link to="/merchants/login" className="nav-link">
-        <div className='the-button'><Button>Mulai Jual</Button></div>
-      </Link>
-      <Link to="/users/login" className="nav-link">
-      <div className='the-button'><Button>Mulai Pesan</Button></div>
-      </Link>
+    <div className='blue-background'>
+    <Header as='h1'><div className='tagline'>Pesan dan Jual Galon Lebih Mudah</div></Header>
+    
+      <div className='button-landing'>
+        <Link to="/merchants/login" className="nav-link">
+          <div className='the-button'><Button>Mulai Jual</Button></div>
+        </Link>
+        <Link to="/users/login" className="nav-link">
+          <div className='the-button'><Button>Mulai Pesan</Button></div>
+        </Link>
+      </div>
     </div>
   </div>
 )
