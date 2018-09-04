4d8228d4a116cc81a353ca0f8f47b17f7d8c9d14
Indro Lie <Indrolie@gmail.com>
2018-09-04 16:32:00 +0700

    Enable user to view previous order and also on going order list

--- src/Pages/Transaction/Users/OrderStatus/index.js
+++ src/Pages/Transaction/Users/OrderStatus/index.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
-import { Header, Form, Divider, Icon, Grid, Button } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
+import { Header, Form, Divider, Icon, Grid, Button, List } from 'semantic-ui-react'
 
-import MenuLogin from '../../../../MenuLogin'
 import { getLocalstorage } from '../../../../function/Localstorage'
 import getOrders from '../../../../function/GetOrders'
 import UserButton from './Button'
@@ -11,28 +9,33 @@ export default class OrderStatus extends Component {
   constructor(props) {
     super(props)
 
-    const order = getLocalstorage('Order')
     this.state = {
       quantities: '',
-      store_name: order.store_name,
-      status: order.status,
-      interval: ''
+      store_name: '',
+      status: '',
+      interval: '',
+      orderList: [],
+      showDetails: false
     }
   }
 
   fetchOrders = async () => {
-    const orderStatus = async () => {
-      const account = await getLocalstorage('Account')
-      const order = await getLocalstorage('Order')
-      const response = await getOrders(`/orders/order/${order.id}`, account.token)
+    const orderList = async () => {
+      const data = await getLocalstorage('Account')
+      const orders = await getOrders('/orders', data.token)
 
-      this.setState({
-        quantities: response.data.quantities,
-        store_name: response.data.store_name,
-        status: response.data.status
+      const orderList = orders.data.map(order => {
+        return {
+          id: order.idorder,
+          fullname: order.fullname,
+          quantities: order.quantities,
+          status: order.status
+        }
       })
+      await this.setState({ orderList })
     }
-    await orderStatus()
+
+    orderList()
   }
 
   componentDidMount = async () => {
@@ -45,28 +48,74 @@ export default class OrderStatus extends Component {
     clearInterval(this.state.interval)
   }
 
+  handleClick = async (id) => {
+    const account = await getLocalstorage('Account')
+
+    const response = await getOrders(`/orders/order/${id}`, account.token)
+    const order = response.data
+
+    this.setState({
+      quantities: order.quantities,
+      status: order.status,
+      store_name: order.store_name,
+      showDetails: true
+    })
+  }
+
+  handleReturn = () => {
+    this.setState({
+      showDetails: false
+    })
+  }
+
   render() {
-    return (
-      <MenuLogin>
-        <Header as='h3' icon textAlign='center'>
-          <Icon name='users' circular />
-          <Header.Content>
-            Pesanan {this.state.quantities} galon dari {this.state.store_name}
-          </Header.Content>
-        </Header>
-        <label>Status: {this.state.status}</label>
-        <Divider />
-        <Form>
-          <Grid.Column floated='left' width={5} className='button-return'>
-            <Link to='/users/transaction'>
-              <Button>Kembali</Button>
-            </Link>
-          </Grid.Column>
-          <UserButton>
-            {this.state.status}
-          </UserButton>
-        </Form>
-      </MenuLogin >
-    )
+    let view = ''
+
+    if (this.state.showDetails) {
+      view = (
+        <div>
+          <Header as='h3' icon textAlign='center'>
+            <Icon name='users' circular />
+            <Header.Content>
+              Pemesanan sebanyak {this.state.quantities} galon dari {this.state.store_name}
+            </Header.Content>
+          </Header>
+          <label>Status: {this.state.status}</label>
+          <Divider />
+          <Form>
+            <Grid.Column floated='left' width={5} className='button-return'>
+              <Button onClick={this.handleReturn}>Kembali</Button>
+            </Grid.Column>
+            <UserButton>
+              {this.state.status}
+            </UserButton>
+          </Form>
+        </div>
+      )
+    } else {
+      view = (
+        <div>
+          {this.state.orderList.map((order, index) => {
+            if (order.status !== 'done') {
+              return (
+                <List divided relaxed>
+                  <List.Item key={index}>
+                    <span onClick={() => this.handleClick(order.id)}>
+                      <List.Icon name='tint' size='large' />
+                      <List.Content>
+                        <List.Header as='a'>{order.fullname} memesan sebanyak {order.quantities} galon</List.Header>
+                        <List.Description as='a'>Status: {order.status}</List.Description>
+                      </List.Content>
+                    </span>
+                  </List.Item>
+                </List>
+              )
+            }
+          })}
+        </div>
+      )
+    }
+
+    return view
   }
 }
