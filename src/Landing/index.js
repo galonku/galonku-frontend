ecdf840a7e8004a9057a63ae81a13ed9112a4d27
unknown <email@abbayosua.com>
2018-09-04 15:17:39 +0700

    Improve Typing Animation on Landing Page

--- src/Landing/index.js
+++ src/Landing/index.js
@@ -11,11 +11,19 @@ const Landing = () => (
     <div className='blue-background'>
       <Header as='h1'>
         <div className='tagline'>
-        <Typing>          
+        <Typing speed={45}>          
           Pesan dan Jual Galon Lebih Mudah
+          <Typing.Backspace count={32} delay={2000} />
+          Beli Galon tanpa <u>Tunggu lama</u>
+        <Typing.Backspace count={11} delay={2000}/>
+          <u>capek angkat galon</u>
+        <Typing.Backspace count={38} delay={4000}/>
+          Jualan Galon dapat Customer <u>lebih banyak</u>
+        <Typing.Backspace count={51} delay={2000}/>
+          Ayo <u>Mulai Coba Galonku</u> Sekarang
+        <Typing.Reset count={3} delay={5000000} />
         </Typing>
         </div>
-      
       </Header>
 
       <div className='button-landing'>
