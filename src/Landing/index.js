b6c5b1d24b3afa0bc08c36a4d007088ebeb2a765
Indro Lie <Indrolie@gmail.com>
2018-09-04 21:57:46 +0700

    Add package for typing animation on landing page

--- src/Landing/index.js
+++ src/Landing/index.js
@@ -1,28 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button, Header } from 'semantic-ui-react'
-import Typing from 'react-typing-animation';
+import Typing from 'react-typing-animation'
 
 import './index.css'
 
 const Landing = () => (
-  
+
   <div className='landing'>
     <div className='blue-background'>
       <Header as='h1'>
         <div className='tagline'>
-        <Typing speed={45}>          
-          Pesan dan Jual Galon Lebih Mudah
-          <Typing.Backspace count={32} delay={2000} />
-          Beli Galon tanpa <u>Tunggu lama</u>
-        <Typing.Backspace count={11} delay={2000}/>
-          <u>capek angkat galon</u>
-        <Typing.Backspace count={38} delay={4000}/>
-          Jualan Galon dapat Customer <u>lebih banyak</u>
-        <Typing.Backspace count={51} delay={2000}/>
-          Ayo <u>Mulai Coba Galonku</u> Sekarang
-        <Typing.Reset count={3} delay={5000000} />
-        </Typing>
+          <Typing speed={45}>
+            Pesan dan Jual Galon Lebih Mudah
+            <Typing.Backspace count={32} delay={2000} />
+            Beli Galon tanpa <u>Tunggu lama</u>
+            <Typing.Backspace count={11} delay={2000} />
+            <u>capek angkat galon</u>
+            <Typing.Backspace count={38} delay={4000} />
+            Jualan Galon dapat Customer <u>lebih banyak</u>
+            <Typing.Backspace count={51} delay={2000} />
+            Ayo <u>Mulai Coba Galonku</u> Sekarang
+            <Typing.Reset count={3} delay={5000000} />
+          </Typing>
         </div>
       </Header>
 
@@ -32,7 +32,7 @@ const Landing = () => (
             <Button className='the-button' inverted size='big'>Mulai Jual</Button>
           </Link>
         </div>
-        
+
         <div className="button-divider">
           <Link to="/users/login" className="nav-link">
             <Button className="the-button" inverted size='big'>Mulai Pesan</Button>
