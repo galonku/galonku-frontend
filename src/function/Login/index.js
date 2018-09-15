a63f38deeb89612ffff8aee89b1b4b751c7d99cc
ariebrainware <ariebrainware@gmail.com>
2018-09-14 22:31:25 -0400

    Merge branch master

--- src/function/Login/index.js
+++ src/function/Login/index.js
@@ -1,14 +1,16 @@
 import request from '../../helpers/axios'
 
 const Login = (URL, data) => {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve,reject) => {
     request
       .post(URL, data)
       .then(response => {
         resolve(response)
+        
       })
       .catch(error => {
-        reject(error)
+        return('Wrong username or password')
+        
       })
   })
 }
